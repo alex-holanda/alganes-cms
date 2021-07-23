b6e37a9c8c4218a953b9f4efc6ed4d5e6920ee55
Alex Holanda <alex.holanda@outlook.com>
2021-07-23 11:12:01 -0300

    Criado componente Typography para renderizar parecido com switch case

---
+++ src/stories/Heading.stories.tsx
@@ -0,0 +1,33 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Heading } from "../components/Typography";
+
+export default {
+  title: "Example/Heading",
+  component: Heading,
+  // argTypes: {
+  //   backgroundColor: { control: "color" },
+  // },
+} as ComponentMeta<typeof Heading>;
+
+const Template: ComponentStory<typeof Heading> = (args) => (
+  <Heading {...args} />
+);
+
+export const Heading1 = Template.bind({});
+Heading1.args = {
+  level: 1,
+  children: "Heading1",
+};
+
+export const Heading2 = Template.bind({});
+Heading2.args = {
+  level: 2,
+  children: "Heading2",
+};
+
+export const Heading3 = Template.bind({});
+Heading3.args = {
+  level: 3,
+  children: "Heading3",
+};
