350a565f6eb7f318dca82158a3d20a7d0cddba64
Alex Holanda <alex.holanda@outlook.com>
2021-07-26 17:32:08 -0300

    Cofigurado estilização do Chart

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,23 +1,25 @@
 import { Line } from "react-chartjs-2";
 
 const data: Chart.ChartData = {
-  labels: ["1", "2", "3", "4", "5", "6"],
+  labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun"],
   datasets: [
     {
-      label: "# of Votes",
+      label: "Receitas",
       data: [12, 19, 3, 5, 2, 3],
-      fill: false,
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgba(255, 99, 132, 0.2)",
-      yAxisID: "y-axis-1",
+      fill: true,
+      backgroundColor: "#09f",
+      borderColor: "#09f",
+      borderWidth: 0.5,
+      yAxisID: "cashflow",
     },
     {
-      label: "# of No Votes",
-      data: [1, 2, 1, 1, 2, 2],
-      fill: false,
-      backgroundColor: "rgb(54, 162, 235)",
-      borderColor: "rgba(54, 162, 235, 0.2)",
-      yAxisID: "y-axis-2",
+      label: "Despesas",
+      data: [1, 2, 1, 4, 7, 9],
+      fill: true,
+      backgroundColor: "#274060",
+      borderColor: "#274060",
+      borderWidth: 0.5,
+      yAxisID: "cashflow",
     },
   ],
 };
@@ -38,21 +40,20 @@ const options: Chart.ChartOptions = {
     },
   },
   scales: {
-    yAxes: [
+    xAxes: [
       {
-        type: "linear",
         display: true,
-        position: "left",
-        id: "y-axis-1",
-      },
-      {
-        type: "linear",
-        display: true,
-        position: "right",
-        id: "y-axis-2",
         gridLines: {
           display: false,
         },
+      },
+    ],
+    yAxes: [
+      {
+        type: "linear",
+        display: false,
+        position: "left",
+        id: "cashflow",
       },
     ],
   },
