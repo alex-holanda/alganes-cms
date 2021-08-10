5855e0bee507e45422a294e99bae7c85e3ee4e47
Alex Holanda <alex.holanda@outlook.com>
2021-08-10 09:40:47 -0300

    Estilizado o componente TagInput

--- src/stories/TagInput.stories.tsx
+++ src/stories/TagInput.stories.tsx
@@ -19,3 +19,17 @@ Default.args = {
   placeholder: "Insira as tags deste post",
   tags: [{ id: "1", text: "JavaScript" }],
 };
+
+export const VariousTags = Template.bind({});
+VariousTags.args = {
+  placeholder: "Insira as tags deste post",
+  tags: [
+    { id: "1", text: "JavaScript" },
+    { id: "2", text: "JAVA" },
+    { id: "3", text: "Ruby on Rails" },
+    { id: "4", text: "Python" },
+    { id: "5", text: "C#" },
+    { id: "6", text: "ReactJS" },
+    { id: "7", text: "Angular" },
+  ],
+};
