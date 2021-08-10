4ee990dc5c813f3aaca646145cbc550c18096e5c
Alex Holanda <alex.holanda@outlook.com>
2021-08-10 20:06:44 -0300

    Utilizado o componente Link do react-router-dom

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,29 +1,14 @@
-import { MouseEvent } from "react";
-import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
 
 export function Navbar() {
-  const history = useHistory();
-
-  function handleAnchorClick(event: MouseEvent<HTMLAnchorElement>) {
-    event.preventDefault();
-    const newRoute = event.currentTarget.getAttribute("href");
-    if (newRoute) {
-      history.push(newRoute);
-    }
-  }
-
   return (
     <nav>
       <ul>
         <li>
-          <a onClick={handleAnchorClick} href="/">
-            Home
-          </a>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <a onClick={handleAnchorClick} href="/contato">
-            Contato
-          </a>
+          <Link to="/contato">Contato</Link>
         </li>
       </ul>
     </nav>
