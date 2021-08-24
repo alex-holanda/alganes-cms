5868a2f15a4d76b78985ae765f81f2380c6b3d92
Alex Holanda <alex.holanda@outlook.com>
2021-08-24 09:07:22 -0300

    Criado classe de serviço

---
+++ src/sdk/@types/index.ts
@@ -0,0 +1,7 @@
+export type { CashFlow } from "./CashFlow";
+export type { Metric } from "./Metric";
+export type { Payment } from "./Payment";
+export type { Post } from "./Post";
+export type { User } from "./User";
+
+export type { AlgaNews } from "./AlgaNews";
