c21fc79d3c1a7cd45a1d53a0ff33c06702443711
Alex Holanda <alex.holanda@outlook.com>
2021-08-26 15:34:39 -0300

    Criado a classe UserService e listado os editores na feature do EditorsList

--- src/app/components/Profile/index.tsx
+++ src/app/components/Profile/index.tsx
@@ -4,12 +4,13 @@ interface ProfileProps {
   name: string;
   description: string;
   editorId: number;
+  avatarUrl?: string;
 }
 
 export function Profile(props: ProfileProps) {
   return (
     <Wrapper tabIndex={0} to={`/editores/${props.editorId}`}>
-      <Avatar src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" />
+      <Avatar src={props.avatarUrl} />
       <Info>
         <Name>{props.name}</Name>
         <Description>{props.description}</Description>
--- src/app/features/EditorsList.tsx
+++ src/app/features/EditorsList.tsx
@@ -1,59 +1,33 @@
+import { useState } from "react";
 import { useEffect } from "react";
+
 import styled from "styled-components";
-import PostService from "../../sdk/services/Post.service";
+import { User } from "../../sdk/@types";
+
+import UserService from "../../sdk/services/User.service";
+
 import { Profile } from "../components/Profile";
 
 export function EditorsList() {
+  const [editors, setEditors] = useState<User.EditorSummary[]>([]);
+
   useEffect(() => {
-    PostService.getAllPosts({
-      editorId: 1,
-      sort: ["id", "desc"],
-      size: 2,
-    });
+    UserService.getAllEditors().then(setEditors);
   }, []);
 
   return (
     <EditorsListWrapper>
-      <Profile
-        editorId={1}
-        name="Daniel Bonifácio"
-        description="Editor há 8 anos"
-      />
-      <Profile
-        editorId={2}
-        name="Daniel Benedito"
-        description="Editor há 2 anos"
-      />
-      <Profile
-        editorId={3}
-        name="Daniel João Frango"
-        description="Editor há 5 anos"
-      />
-      <Profile
-        editorId={4}
-        name="Camila Vasconcellos"
-        description="Editor há 12 anos"
-      />
-      <Profile
-        editorId={5}
-        name="Gabriel Freitas"
-        description="Editor há 1 ano"
-      />
-      <Profile
-        editorId={6}
-        name="Rodrigo Mendes"
-        description="Editor há 9 anos"
-      />
-      <Profile
-        editorId={7}
-        name="Aline Texeira"
-        description="Editor há 12 anos"
-      />
-      <Profile
-        editorId={8}
-        name="Amanda Rodrigues"
-        description="Editor há 1 ano"
-      />
+      {editors.map((editor) => {
+        return (
+          <Profile
+            key={editor.id}
+            editorId={editor.id}
+            name={editor.name}
+            description="Editor há X meses"
+            avatarUrl={editor.avatarUrls.small}
+          />
+        );
+      })}
     </EditorsListWrapper>
   );
 }
