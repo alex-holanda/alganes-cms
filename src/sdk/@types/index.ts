12fe7bebed53cb2699408bc88065fbc9f252da38
Alex Holanda <alex.holanda@outlook.com>
2021-08-26 11:19:34 -0300

    Criado classe FileService

--- src/sdk/@types/index.ts
+++ src/sdk/@types/index.ts
@@ -3,5 +3,6 @@ export type { Metric } from "./Metric";
 export type { Payment } from "./Payment";
 export type { Post } from "./Post";
 export type { User } from "./User";
+export type { File } from "./File";
 
 export type { AlgaNews } from "./AlgaNews";
