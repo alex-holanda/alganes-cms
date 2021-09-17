2103fb7bc339a6b84f727f72a70f89c303535d95
Alex Holanda <alex.holanda@outlook.com>
2021-09-17 14:21:27 -0300

    Disparando uma ação

--- src/app/views/Home.view.tsx
+++ src/app/views/Home.view.tsx
@@ -1,3 +1,7 @@
+import { useDispatch } from "react-redux";
+
+import { addPost } from "../../core/store/Post.slice";
+
 import { usePageTitle } from "../../core/hooks/usePageTitle";
 
 import DefaultLayout from "../layouts/Default";
@@ -7,9 +11,54 @@ import UserPerformance from "../features/UserPerformance";
 import { UserTopTags } from "../features/UserTopTags";
 import { UserEarnings } from "../features/UserEarnings";
 import ErrorBoundary from "../components/ErrorBoundary";
+import { useEffect } from "react";
+
+const fakePost = {
+  id: 42,
+  slug: "como-fazer-x-coisas-com-react-js",
+  title: "Como fazer X coisas com React.js",
+  imageUrls: {
+    default:
+      "https://storage.googleapis.com/alganews-files/posts/avatar-joao.jpeg",
+    small:
+      "https://storage.googleapis.com/alganews-files/posts/avatar-joao-small.jpeg",
+    medium:
+      "https://storage.googleapis.com/alganews-files/posts/avatar-joao-medium.jpeg",
+    large:
+      "https://storage.googleapis.com/alganews-files/posts/avatar-joao-large.jpeg",
+  },
+  editor: {
+    id: 29,
+    name: "Daniel Bonifacio",
+    avatarUrls: {
+      default:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao.jpeg",
+      small:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-small.jpeg",
+      medium:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-medium.jpeg",
+      large:
+        "https://storage.googleapis.com/alganews-files/posts/avatar-joao-large.jpeg",
+    },
+    createdAt: "2017-03-04T00:12:45Z",
+  },
+  createdAt: "2020-12-04T00:12:45-03:00",
+  updatedAt: "2020-12-05T00:12:45-03:00",
+  published: true,
+  tags: ["JavaScript"],
+  canBePublished: true,
+  canBeUnpublished: true,
+  canBeDeleted: true,
+  canBeEdited: true,
+};
 
 export function HomeView() {
   usePageTitle("Home");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(addPost(fakePost));
+  }, [dispatch]);
 
   return (
     <DefaultLayout>
